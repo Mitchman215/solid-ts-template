f4d69b19dfa200bc3b815c9e19a7aa899d7764d3
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-06-01 00:17:24 -0400

    enhanced starter configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
+/* eslint-enable */
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
   },
-});
+})
